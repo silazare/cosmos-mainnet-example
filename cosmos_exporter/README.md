b6990deae55ea7ab68c462ace06c4272506af2ad
Svyatoslav Lazarev <silazare.nsk@gmail.com>
2021-06-08 10:04:51 +0200

    Change: prometheus exporter

--- cosmos_exporter/README.md
+++ cosmos_exporter/README.md
@@ -2,6 +2,8 @@
 
 ## Build steps
 
+1) Build binary
+
 ```shell
 go mod init cosmos_exporter
 go get github.com/prometheus/client_golang
@@ -9,8 +11,34 @@ go get
 go build
 ```
 
-## Exposed metrics
+2) Move binary into `/home/cosmos/go/bin` folder
 
-1) Number of peers in blockchain network
-2) TBD
-3) TBD
+## Usage example
+
+1) Create a service file from example
+
+```shell
+/etc/systemd/system/cosmos-exporter.service
+```
+
+2) Install systemd service
+
+```shell
+sudo systemctl daemon-reload
+sudo systemctl enable cosmos-exporter.service
+sudo systemctl start cosmos-exporter.service
+sudo systemctl status cosmos-exporter.service
+journalctl -e -u cosmos-exporter.service
+```
+
+3) Scrape endpoint
+
+```shell
+curl -s localhost:9201/metrics | grep -v \# | grep cosmos
+```
+
+## Exposed Cosmos metrics
+
+- Number of peers in blockchain network
+- Current block number
+- Time out of sync between current block time and time now
--- cosmos_exporter/main.go
+++ cosmos_exporter/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
- //"fmt"
  "encoding/json"
  "io/ioutil"
  "log"
@@ -12,18 +11,19 @@ import (
  "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-// Inner struct for JSON /net_info 'n_peers'
-type Peers struct {
-	Peers string `json:"n_peers"`
-}
-
-// Outer struct for JSON /net_info 'result'
-type Result struct {
-	Result Peers `json:"result"`
-}
-
 // Get number of blockchain peers from REST API of Gaia
 func getNumberPeers() float64 {
+
+	// Inner struct for JSON /net_info 'n_peers'
+	type Peers struct {
+		Peers string `json:"n_peers"`
+    }
+
+    // Outer struct for JSON /net_info 'result'
+	type Result struct {
+		Result Peers `json:"result"`
+    }
+
 	url := "http://localhost:26657/net_info"
 
 	gaiaClient := http.Client{
@@ -59,20 +59,155 @@ func getNumberPeers() float64 {
 	return float64(numberPeersValue)
 }
 
+// Get current block number from REST API of Gaia
+func getCurrentBlock() float64 {
+
+	// Inner struct for JSON /status 'block'
+	type Block struct {
+		Block string `json:"block"`
+    }
+
+	// Inner struct for JSON /status 'protocol_version'
+	type ProtocolVersion struct {
+		ProtocolVersion Block `json:"protocol_version"`
+    }
+
+	// Inner struct for JSON /status 'node_info'
+	type NodeInfo struct {
+		NodeInfo ProtocolVersion `json:"node_info"`
+    }
+
+    // Outer struct for JSON /status 'result'
+	type Result struct {
+		Result NodeInfo `json:"result"`
+    }
+
+	url := "http://localhost:26657/status"
+
+	gaiaClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := gaiaClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var block_number Result
+	jsonErr := json.Unmarshal(body, &block_number)
+        if jsonErr != nil {
+		log.Fatalf("Unable to parse value: %q, error: %s", string(body), jsonErr.Error())
+	}
+
+	blockNumberValue, err := strconv.ParseFloat(block_number.Result.NodeInfo.ProtocolVersion.Block, 64)
+	return float64(blockNumberValue)
+}
+
+// Get time for current block from REST API of Gaia
+func getCurrentBlockTime() string {
+
+	// Inner struct for JSON /status 'protocol_version'
+	type BlockTime struct {
+		BlockTime string `json:"latest_block_time"`
+    }
+
+	// Inner struct for JSON /status 'sync_info'
+	type SyncInfo struct {
+		SyncInfo BlockTime `json:"sync_info"`
+    }
+
+    // Outer struct for JSON /status 'result'
+	type Result struct {
+		Result SyncInfo `json:"result"`
+    }
+
+	url := "http://localhost:26657/status"
+
+	gaiaClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, getErr := gaiaClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	var block_time Result
+	jsonErr := json.Unmarshal(body, &block_time)
+        if jsonErr != nil {
+		log.Fatalf("Unable to parse value: %q, error: %s", string(body), jsonErr.Error())
+	}
+
+	return string(block_time.Result.SyncInfo.BlockTime)
+}
+
+// Calculate time difference in seconds
+func getTimeDifference(timestamp string) float64 {
+
+	// Get time now
+	dt1 := time.Now()
+	// Get current block time and convert into Time struct
+	layout := "2006-01-02T15:04:05Z"
+	dt2, err := time.Parse(layout, timestamp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return (dt1.Sub(dt2).Seconds())
+}
+
 //Define a struct for the collector that contains pointers
 //to prometheus descriptors for each metric you wish to expose.
 //Note you can also include fields of other types if they provide utility
 //but we just won't be exposing them as metrics.
 type cosmosCollector struct {
 	numberPeersMetric *prometheus.Desc
+	blockNumberMetric *prometheus.Desc
+	blockTimeSyncMetric *prometheus.Desc
 }
 
 //You must create a constructor for you collector that
 //initializes every descriptor and returns a pointer to the collector
 func newCosmosCollector() *cosmosCollector {
 	return &cosmosCollector{
-		numberPeersMetric: prometheus.NewDesc("cosmos_n_peers",
+		numberPeersMetric: prometheus.NewDesc("cosmos_peers_number",
 			"Number of peers in blockchain network",
+			nil, nil,
+		),
+		blockNumberMetric: prometheus.NewDesc("cosmos_block_number",
+			"Current block number at the node",
+			nil, nil,
+		),
+		blockTimeSyncMetric: prometheus.NewDesc("cosmos_block_time_out_sync_seconds",
+			"Time out of sync between current block time and time now",
 			nil, nil,
 		),
 	}
@@ -84,6 +219,8 @@ func (collector *cosmosCollector) Describe(ch chan<- *prometheus.Desc) {
 
 	//Update this section with the each metric you create for a given collector
 	ch <- collector.numberPeersMetric
+	ch <- collector.blockNumberMetric
+	ch <- collector.blockTimeSyncMetric
 }
 
 //Collect implements required collect function for all promehteus collectors
@@ -91,11 +228,15 @@ func (collector *cosmosCollector) Collect(ch chan<- prometheus.Metric) {
 
 	//Implement logic here to determine proper metric value to return to prometheus
 	//for each descriptor or call other functions that do so.
-	metricValue := getNumberPeers()
+	numberPeersMetricValue := getNumberPeers()
+	blockNumberMetricValue := getCurrentBlock()
+	blockTimeSyncMetricValue := getTimeDifference(getCurrentBlockTime())
 
 	//Write latest value for each metric in the prometheus metric channel.
 	//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
-	ch <- prometheus.MustNewConstMetric(collector.numberPeersMetric, prometheus.CounterValue, metricValue)
+	ch <- prometheus.MustNewConstMetric(collector.numberPeersMetric, prometheus.CounterValue, numberPeersMetricValue)
+	ch <- prometheus.MustNewConstMetric(collector.blockNumberMetric, prometheus.CounterValue, blockNumberMetricValue)
+	ch <- prometheus.MustNewConstMetric(collector.blockTimeSyncMetric, prometheus.CounterValue, blockTimeSyncMetricValue)
 	log.Println("Endpoint scraped")
 
 }
